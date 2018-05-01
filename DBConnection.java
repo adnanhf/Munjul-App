e8b2008aa5eee7d8ad5082c599409a0186579a3b
Adnan H.F <adnanhasanfalah@gmail.com>
2018-05-01 09:21:12 +0700

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,38 @@
+package changeToYourPackageName;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class DBConnection {
+    private Connection con;
+    
+    public Connection getKoneksi(){
+        return con;
+    }
+    
+    public void connectDatabase(){
+        try{
+            //load the driver class  
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            try{
+                String url,username,password;
+                url = "jdbc:oracle:thin:@localhost:1521:XE";
+                username = "yourusername";
+                password = "yourpassword";
+                koneksi = DriverManager.getConnection(url,username,password);
+                //Confirmation that Java has been successfully connected with oracl xe
+                System.out.println("Connected!");
+            }catch(SQLException sqle){
+                JOptionPane.showMessageDialog(null, "Connection Failed! " + sqle.getMessage());
+                System.exit(0);
+            }
+        }catch(ClassNotFoundException cnfe){
+            JOptionPane.showMessageDialog(null, "Driver Not Found! \n" + cnfe.getMessage());
+            System.exit(0);
+        }
+    }
+    
+    public static void main(String[] kon) {  
+        new DBConnection().connectDatabase();
+    } 
+}
